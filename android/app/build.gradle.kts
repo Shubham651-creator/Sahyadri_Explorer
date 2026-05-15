7b237ea10dcec6e67ccedaabdde62d020a28661a
Shubham651-creator <shubhamawale651@gmail.com>
2026-05-15 13:38:47 +0530

    refactor(branding): update app identity, package name, and Firebase config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
 }
 
 android {
-    namespace = "com.example.sahyadri_explorer"
+    namespace = "com.zenitrek.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -38,7 +38,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.sahyadri_explorer"
+        applicationId = "com.zenitrek.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
